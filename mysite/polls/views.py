a755503dc1e9009444d3a1d6aa353c1097a83776
SoliatOmolara <azeezsoleehah@gmail.com>
2023-04-03 23:27:43 +0100

    Updating files

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,20 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("Hello! Welcome to my first django view")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at the question %s.", question_id)
+
+def results(request,question_id):
+    response = "You're looking at the result %s."
+    return HttpResponse(response, question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're looking at the vote %s", question_id)
